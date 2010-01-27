36884f18afae534d30a6b5eabb6384254d574858
Jonas Gehring <jonas.gehring@boolsoft.org>
2010-01-27 22:15:10 +0100

    Initial commit - set up build system

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * pgpry alpha - PGP private key password recovery
+ * Copyright (C) 2010 Jonas Gehring
+ */
+
+
+#include <cstdlib>
+#include <iostream>
+
+
+// Program entry point
+int main(int argc, char **argv)
+{
+	std::cout << "hello world ..." << std::endl;
+	return EXIT_SUCCESS;
+}
