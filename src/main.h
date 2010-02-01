ba59eef54ee55b539fd896e6818cb321be9ce070
Jonas Gehring <jonas.gehring@boolsoft.org>
2010-02-01 21:36:12 +0100

    Added additional checks to configure.ac

--- src/main.h
+++ src/main.h
@@ -14,15 +14,27 @@
 #include "config.h"
 
 
-#ifndef HAVE_STDINT_H
- typedef signed char int8_t;
- typedef unsigned char uint8_t;
- typedef signed short int16_t;
- typedef unsigned short uint16_t;
- typedef signed int int32_t;
- typedef unsigned int uint32_t;
-#else
+// Standard integer types
+#ifdef HAVE_STDINT_H
  #include <stdint.h>
+#endif
+#ifndef int8_t
+ typedef signed char int8_t;
+#endif
+#ifndef uint8_t
+ typedef unsigned char uint8_t;
+#endif
+#ifndef int16_t
+ typedef signed short int16_t;
+#endif
+#ifndef uint16_t
+ typedef unsigned short uint16_t;
+#endif
+#ifndef int32_t
+ typedef signed int int32_t;
+#endif
+#ifndef uint32_t
+ typedef unsigned int uint32_t;
 #endif
 
 
